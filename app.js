2eccd9fb003ce0ba35cf81adade9eef4b6b10d0f
Albenoo <albeniskerqeli5@gmail.com>
2020-06-01 23:39:29 +0200

    Website UI Design v2.0

---
+++ app.js
@@ -0,0 +1,18 @@
+function toggler() {
+  let togglenav = document.getElementById("nav");
+
+  if (togglenav.style.display === "flex" ) {
+    togglenav.style.display = "none";
+
+  } else {
+    togglenav.style.display = "flex";
+
+  }
+   
+
+
+
+}
+
+
+
